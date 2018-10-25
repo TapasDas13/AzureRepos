4e163b6889b4cd492fc6f1f5e7959f0f6e0aa96f
TapasDas13 <tapas.diatm@gmail.com>
2018-10-25 05:26:17 -0400

    commit

---
+++ FirstProjectAzure/src/com/test/FirstAzure.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class FirstAzure {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my first Azure Test Project");
+	}
+
+}
