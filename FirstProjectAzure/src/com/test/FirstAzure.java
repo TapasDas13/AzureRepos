f6af20a82eea78c1f03a08e7aeff909ebab3ffb1
TapasDas13 <44463158+TapasDas13@users.noreply.github.com>
2018-10-25 15:01:13 +0530

    Update FirstAzure.java

--- FirstProjectAzure/src/com/test/FirstAzure.java
+++ FirstProjectAzure/src/com/test/FirstAzure.java
@@ -7,7 +7,7 @@ public class FirstAzure {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("This is my first Azure Test Project");
+System.out.println("This is my first Azure Test Project 123");
 	}
 
 }
